6c0501a38bca7071d0bd191a13c4aeb3218a9f99
Justin Aquino <justin.aquino25@gmail.com>
2023-01-20 15:46:53 -0800

    initial commit

---
+++ strings.rb
@@ -0,0 +1,50 @@
+# # puts 'Hello, World!'
+# # p 'hello?'
+# # print 'Hi!'
+
+# # # pretty much like a console.log
+
+# # # for multi line comments
+# # # we use =begin
+# # # and =end
+
+# # name = 'Justin'
+
+# # twitch = 'twitch.tv/jiji_jichu4'
+
+# # age = 23
+
+# # print "Hello #{name}"
+# # puts "I am #{age}"
+
+# # print "Hello, "
+
+# # print 'Enter your name: '
+# # name = gets.chomp # this is to get user input
+# # puts "Hello #{name}, enter your age: "
+# # age = gets.chomp.to_i # to_i method converts input to integer.
+# # puts "My name is #{name} and I am #{age} years old."
+
+# # ##Strings
+
+# str1 = 'This is a string'
+# str2 = 'This is another string'
+
+# # puts str1
+# # puts str2
+
+# # # compare strings
+
+# # puts str1 == str2 # output false, since they are not equal.
+
+# puts str1[0] # output is the first character of the string.
+# puts str1[0..5] # output is the first 6 characters of the string
+# puts str1[0, 4] # output the first 4 characters
+# puts str1.length # outputs the number of characters in the string.
+# puts str1[0, str1.length] # starts from 0 to 16, which is the number of characters in the string.
+
+### ARRAY METHODS
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+puts arr1[1]
