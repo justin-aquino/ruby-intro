9135ead650e58c94a3cdc0ff314225417a615d2b
Justin Aquino <justin.aquino25@gmail.com>
2023-01-23 12:35:15 -0800

    lesson1 added

---
+++ lesson1.rb
@@ -0,0 +1,6 @@
+#Ruby is an object-oriented programming(OOP) language. 
+=begin
+The OOP paradigm views a program as a colelction of objects that communicate with one another.
+An object is a digital data structure. It's a container that stores data and provides ways to access and manipulate that data.
+In Ruby, everything is an object.
+=end
