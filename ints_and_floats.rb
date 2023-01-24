4fe99359cfd511c5ef9ca7970bbd9d279eb1d7bb
Justin Aquino <justin.aquino25@gmail.com>
2023-01-23 18:01:16 -0800

    ints and floats

---
+++ ints_and_floats.rb
@@ -0,0 +1,18 @@
+puts 1_000 #use an underscore instead of a comma to separate the numbers out to make it easier to read. Doesn't show in the output.
+
+#floating numbers are numbers with decimals.
+
+puts 3.14 
+
+#decimals need to be prefixed.
+
+puts 0.14 #there has to be a zero before the decimal.
+
+
+=begin
+Arithmetic operators in Ruby is pretty much the same as in Javascript, except when dividing.
+=end
+
+puts 11 / 5 #outputs 2. Ruby chops off the floating numbers / decimals.
+puts 11.0 / 5 #unless you add a decimal. output: 2.2
+
