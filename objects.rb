dd68aa47551077d0ffa18e8184f9e8dfcc615de5
Justin Aquino <justin.aquino25@gmail.com>
2023-01-29 16:47:01 -0800

    string methods

--- objects.rb
+++ objects.rb
@@ -1,7 +1,21 @@
 # Intro to Ruby Objects
 
+#String Objects
+
 puts "Hello World".length
 
 puts "hello world".upcase #same as JS toUpperCase method
 
 puts "HELLO WORLD".downcase # same as JS toLowerCase method.
+
+
+greeting = "hello Justin"
+
+
+puts greeting.upcase
+
+
+#Methods are behaviors or functionalities that an object is capable of doing.
+
+puts greeting.upcase() #can work with or without parentheses. But convention is, if you don't need it, don't use it.
+
