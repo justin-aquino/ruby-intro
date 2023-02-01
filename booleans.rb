6cd9c93c5517a443bc7313c39d7989075ea9dde7
Justin Aquino <justin.aquino25@gmail.com>
2023-01-31 17:54:00 -0800

    comparative operators

--- booleans.rb
+++ booleans.rb
@@ -5,6 +5,6 @@
 
 puts 3 < 45
 puts 3 === 2
-puts 2 == "2"
+puts 2 == "2" #not like  JS where this would output true.
 
 puts 2 != 4
---
+++ comparison.rb
@@ -0,0 +1,13 @@
+#Equality Operator
+
+a = 1
+b = 2
+c = 3
+
+puts a == b
+
+puts a == c
+
+#inequality
+
+puts a != b
