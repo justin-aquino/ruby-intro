148fde30090099f81aa1701b55f52bd66f22d3ba
Justin Aquino <justin.aquino25@gmail.com>
2023-02-01 13:42:01 -0800

    more class comparison

--- comparison.rb
+++ comparison.rb
@@ -11,3 +11,6 @@ puts a == c
 #inequality
 
 puts a != b
+
+puts 5.class == 5.0.class #will show false, because 5 is an integer, 5.0 is a float. 
+puts 5 == 5.0 #will output true, since numerically they are the same.
