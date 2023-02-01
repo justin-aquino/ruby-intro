f9df52d28d1dcd310ff0b7a876610718ddf17a71
Justin Aquino <justin.aquino25@gmail.com>
2023-02-01 15:30:47 -0800

    object methods for arithmetic operators

---
+++ arithmethic_methods.rb
@@ -0,0 +1,15 @@
+# + method (dot plus sign method)
+
+
+puts 1 + 2
+puts 5 - 3
+puts 2 * 3
+puts 10 / 3
+
+
+#pretty much arithmetic operators are methods
+puts 1.+(2)
+puts 11.-(2)
+puts 1.*(2)
+puts 1./(2) #this works like division, but uncommon
+puts 1.div(2) #this is more widely used
