235c1e3a977a9bb0ca05deeff1e0e1fd1bd0745d
Justin Aquino <justin.aquino25@gmail.com>
2023-02-01 13:48:18 -0800

    predicate method

--- comparison.rb
+++ comparison.rb
@@ -14,3 +14,4 @@ puts a != b
 
 puts 5.class == 5.0.class #will show false, because 5 is an integer, 5.0 is a float. 
 puts 5 == 5.0 #will output true, since numerically they are the same.
+
---
+++ methods.rb
@@ -0,0 +1,7 @@
+#PREDICATE METHODS - should with a question mark (?) Method will check if it is true or false.
+
+puts 10.odd? #odd is a predicate method, will return true or false. Output: false.
+
+puts 10.positive?
+
+puts -1.negative?
