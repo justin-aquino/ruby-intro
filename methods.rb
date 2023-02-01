79c2e39be72c54d726caafb46bf93128139c905f
Justin Aquino <justin.aquino25@gmail.com>
2023-02-01 15:22:00 -0800

    include method

--- methods.rb
+++ methods.rb
@@ -1,7 +1,17 @@
 #PREDICATE METHODS - should with a question mark (?) Method will check if it is true or false.
 
-puts 10.odd? #odd is a predicate method, will return true or false. Output: false.
+# puts 10.odd? #odd is a predicate method, will return true or false. Output: false.
 
-puts 10.positive?
+# puts 10.positive?
 
-puts -1.negative?
+# puts -1.negative?
+
+# #METHODS WITH ARGUMENTS
+
+# puts "Big Mac".include? "M" #can only accept 1 argument.
+
+array1 = [1,2,3,4,5,6,7,8,9]
+
+puts array1.include? 2 #works in arrays too.
+
+# puts "Big Mac".include?("B", "M") #only accepts 1 argument even when it has a parentheses.
