95f4805c2db4eac7cd01f2d9751dbf991df82dc2
Justin Aquino <justin.aquino25@gmail.com>
2023-02-01 15:45:34 -0800

    float methods

--- arithmethic_methods.rb
+++ arithmethic_methods.rb
@@ -7,9 +7,24 @@ puts 2 * 3
 puts 10 / 3
 
 
-#pretty much arithmetic operators are methods
+#pretty much arithmetic operators are methods (same as .includes method, there are dot + operator methods)
 puts 1.+(2)
 puts 11.-(2)
 puts 1.*(2)
 puts 1./(2) #this works like division, but uncommon
 puts 1.div(2) #this is more widely used
+
+
+###FLOATS METHOD
+
+num = 90
+
+floatie = 1.2345
+
+puts num.floor #rounds DOWN
+puts num.ceil #rounds UP
+
+puts floatie.ceil.class #changes the float to int
+
+puts floatie.round # round method follows math rules.
+puts floatie.round(2) # round method will round up from the second number in the decimal.
