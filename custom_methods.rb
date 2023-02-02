964f799ce0a9adbc2eb4c59e7aec988727753394
Justin Aquino <justin.aquino25@gmail.com>
2023-02-02 15:34:51 -0800

    return values

--- custom_methods.rb
+++ custom_methods.rb
@@ -28,3 +28,19 @@ film_movie
 puts action_star #invalid. action_star's scope is only local. Same as in JS unless you use var.
 
 #unlike in JS, Ruby does not look for variables outside the function. Therefore, any variables outside the function is not accessible within the function.
+
+
+## RETURN VALUE
+
+def add_two_numbers(num1, num2)
+    puts "ok I'm solving your math problems"
+    return num1 + num2
+    #the keyword is optional, since Ruby always return the result of the last expression in the function.
+    #return still terminates the logic, just like in Javascript. A must for conditionals.
+end
+
+
+def nothing
+end
+
+# "nothing" will return Nil = which is a class that represents nothingness. same as null.
