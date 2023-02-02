e9308b08ac033ff45b7ee81b6b00ba6b1e6a33b6
Justin Aquino <justin.aquino25@gmail.com>
2023-02-01 17:45:57 -0800

    custom methods and local variables

--- custom_methods.rb
+++ custom_methods.rb
@@ -8,3 +8,23 @@ end
 
 
 ##PARAMETERS AND ARGUMENTS
+
+def praise_person(name, name2)
+    puts "#{name} is awesome!"
+    puts "#{name2} misses #{name}"
+end
+
+praise_person("Jichu", "Juno")
+
+## LOCAL VARIABLES
+
+def film_movie
+    action_star = "Emma Watson"
+    puts action_star
+end
+
+film_movie
+
+puts action_star #invalid. action_star's scope is only local. Same as in JS unless you use var.
+
+#unlike in JS, Ruby does not look for variables outside the function. Therefore, any variables outside the function is not accessible within the function.
