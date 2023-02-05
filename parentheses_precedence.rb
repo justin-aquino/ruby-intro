d607df2c7830a8126d2e860d4966545feab7f708
Justin Aquino <justin.aquino25@gmail.com>
2023-02-05 15:26:34 -0800

    meal prep nested if-else

--- parentheses_precedence.rb
+++ parentheses_precedence.rb
@@ -11,5 +11,29 @@ authenticate_agent("007","James Bond", "Secret Agents")
 authenticate_agent("007","James Bonde", "Secret Agents")
 
 
+
+#nested if statement
+
 def meal_plan(time_of_week, time_of_day)
+    if time_of_week == "Weekday"
+        if time_of_day == "breakfast"
+            puts "Cereal"
+            
+        elsif time_of_day == "lunch"
+            puts "sandwich"
+        elseif time_of_day == "dinner"
+            puts "Chicken dinner"
+        end
+    elsif time_of_week == "Weekend"
+        if time_of_day == "breakfast"
+            puts "french toast"
+        elsif time_of_day == "lunch"
+            puts "hamburger"
+        elseif time_of_day == "dinner"
+            puts "nicoise salad"
+        end
+    end
+end
     
+
+meal_plan("Weekend", "breakfast")
