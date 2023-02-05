a431171f63cae1183463aa7510483499d0a668fd
Justin Aquino <justin.aquino25@gmail.com>
2023-02-05 14:46:36 -0800

    multiple conditions

--- if_statement.rb
+++ if_statement.rb
@@ -20,12 +20,25 @@
 
 =end
 
-def odd_or_even(num)
-    if num.odd?
-        puts "the number #{num} is odd."
-    else
-        puts "the number #{num} is even"
-    end
-end
+# def odd_or_even(num)
+#     if num.odd?
+#         puts "the number #{num} is odd."
+#     else
+#         puts "the number #{num} is even"
+#     end
+# end
 
-puts odd_or_even(3)
+# puts odd_or_even(3)
+
+age = 25
+ticket = "General Admission"
+id = false
+
+if age >= 21 && ticket == "General Admission" && id
+    puts "This way to gen ad"
+elsif age < 21 && ticket == "General Admission" && id
+    puts "Here's a bracelet for underage no drinking people"
+elsif age >= 21 && ticket == "General Admission" && !id
+    puts "I can't let you in without ID"
+    
+end
