a71727a6428647f2fade73ca8453ded80ce79e5a
Justin Aquino <justin.aquino25@gmail.com>
2023-02-05 14:40:48 -0800

    conditional custom method

--- if_statement.rb
+++ if_statement.rb
@@ -1,7 +1,31 @@
-age = 21
+# age = 21
 
-if age < 21
-    puts "no you can't drink"
-else
-    puts "how many shots?"
-end 
+# if age < 21
+#     puts "no you can't drink"
+# else
+#     puts "how many shots?"
+# end 
+
+
+#truthy vs falsy
+
+=begin
+ strings, 
+ integers,
+ floats,
+ negative ints and floats,
+ ranges,
+ 
+ the only falsy is nil and false.
+
+=end
+
+def odd_or_even(num)
+    if num.odd?
+        puts "the number #{num} is odd."
+    else
+        puts "the number #{num} is even"
+    end
+end
+
+puts odd_or_even(3)
