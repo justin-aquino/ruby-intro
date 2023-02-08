e0c1443ff5020160bab7b85547f7f5b68212b145
Justin Aquino <justin.aquino25@gmail.com>
2023-02-07 22:32:07 -0800

    until keyword

--- iterations.rb
+++ iterations.rb
@@ -6,21 +6,36 @@
 # end
 
 
-status = true
+# status = true
 
-while status 
-    puts "Enter username: "
-    username = gets.chomp.downcase
-    puts "Please enter password: "
-    password = gets.chomp
+# while status 
+#     puts "Enter username: "
+#     username = gets.chomp.downcase
+#     puts "Please enter password: "
+#     password = gets.chomp
 
-    if username == "juno" && password == "Chaeyoung29"
-        puts "Welcome, #{username}, the nuclear codes are..."
-        status = false
-    elsif username == "quit" || password == "quit"
-        puts "Goodbye. This message will self destruct in 3.... 2.... 1..."
-        status = false
-    else
-        puts "Access denied. Nuking your location."
-    end
-end
+#     if username == "juno" && password == "Chaeyoung29"
+#         puts "Welcome, #{username}, the nuclear codes are..."
+#         status = false
+#     elsif username == "quit" || password == "quit"
+#         puts "Goodbye. This message will self destruct in 3.... 2.... 1..."
+#         status = false
+#     else
+#         puts "Access denied. Nuking your location."
+#     end
+# end
+
+
+#UNTIL KEYWORD - a complementary keyword to the while loop
+
+i = 1
+
+# while i < 10
+#     puts i
+#     i += 1
+# end
+
+until i > 9 #essentially will become true once i is 10.
+    puts i 
+    i += 1
+end # this is essentially the same as the above while loop, only in reverse.
