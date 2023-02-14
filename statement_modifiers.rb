503dfa059b3b006bf0af243b50c82817e558d4db
Justin Aquino <justin.aquino25@gmail.com>
2023-02-13 16:44:15 -0800

    inline statements or one liners

---
+++ statement_modifiers.rb
@@ -0,0 +1,11 @@
+number = 5000
+verified = true
+
+# if number > 2500
+#     puts "Huge number!"
+# end
+
+puts "Huge Number!!!" if number > 2500  #if there's only one line you can do this.
+
+puts "Huge Number and verified!!!!" if number > 2500 && verified # works for multiple conditions too
+
