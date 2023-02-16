79d5adeb126feda2867fab4aadcc84315f7dfd26
Justin Aquino <justin.aquino25@gmail.com>
2023-02-15 17:14:20 -0800

    ruby intro to arrays

---
+++ arrays_intro.rb
@@ -0,0 +1,12 @@
+p []
+p [].class #returns Array
+
+numbers = [1,2,3,4,5,6]
+
+puts numbers[1] #same as JavaScript
+
+p numbers #will output the whole array, including the brackets
+
+toys = ["plushie", "nerf gun", "action figures"]
+
+p toys
