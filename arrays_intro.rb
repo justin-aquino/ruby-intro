9539fb6e94936ad0f77495271b938fbad04488a5
Justin Aquino <justin.aquino25@gmail.com>
2023-02-16 17:40:31 -0800

    array of strings short hand

--- arrays_intro.rb
+++ arrays_intro.rb
@@ -18,3 +18,10 @@ puts things
 students = [{name: "justin", age: 28},{name:"Jichu", age: "22"}, {name: "boomboom", age: 27}]
 
 # puts students[0].name# doesnt work like js
+
+#short hand array strings.
+
+names = %w[Justin Jichu Boomboom Moosmoos] #doesnt need to be enclosed in quotation marks and is only separated by spaces. 
+#You can't do two first names, like Justin Bernadette, because it will save it as 2 separate names. 
+
+puts names
