450a8c20928246e50fc037e90a476be8e4f94046
Justin Aquino <justin.aquino25@gmail.com>
2023-02-18 23:34:06 -0800

    array fetch

--- arrays.rb
+++ arrays.rb
@@ -1,19 +1,30 @@
-### ARRAY METHODS
+# ### ARRAY METHODS
 
-arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-puts arr1[1]
+# puts arr1[1]
 
-# ternary operator
+# # ternary operator
 
-a = arr1[1]
-b = arr1[3]
-c = a > b ? a : b
+# a = arr1[1]
+# b = arr1[3]
+# c = a > b ? a : b
 
-puts c
+# puts c
 
 
-# puts arr1.at(3) # output is 4
-# puts arr1.fetch(2) # output is 3. If given a parameter that's more than the length of the array.
-# puts arr1.first # output is the first element in the array.
-puts array3 = arr1.take(7) # outputs the first 4 elements in the array
+# # puts arr1.at(3) # output is 4
+# # puts arr1.fetch(2) # output is 3. If given a parameter that's more than the length of the array.
+# # puts arr1.first # output is the first element in the array.
+# puts array3 = arr1.take(7) # outputs the first 4 elements in the array
+
+
+#ARRAY FETCH
+
+names = ["Jichu", "Justin", "Moosmoos", "Boomboom"]
+
+# p names[2]
+# p names[100]
+
+p names.fetch(2) #pretty much works the same way as bracket 2
+p names.fetch(100) #will return error instead of nil.
